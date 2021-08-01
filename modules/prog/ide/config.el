20a792a125751b9eb9b6ac8a8e16212efb97aece
AmaiKinono <amaikinono@gmail.com>
2021-08-02 02:20:40 +0800

    ide, enh: company config

--- modules/prog/ide/config.el
+++ modules/prog/ide/config.el
@@ -2,6 +2,9 @@
 
 ;; Auto-completion, finding definition, version control, linting, etc.
 
+(require 'cl-lib)
+(require 'rx)
+
 ;; TODO (maybe): debuffer, snippet, repl, help system
 
 ;;; Checker
@@ -16,22 +19,52 @@
 ;;; Auto completion
 
 (use-package company
-  :hook (pre-command-hook . global-company-mode)
+  :hook (pre-command . global-company-mode)
   :config
   (toki/setq
-   company-backends '(company-dabbrev)
+   company-backends '(company-dabbrev-code company-dabbrev)
    company-minimum-prefix-length 2
-   company-dabbrev-char-regexp "[[:alnum:]-_]"
+   company-dabbrev-code-everywhere t
    company-tooltip-align-annotations nil
-   company-tooltip-limit 10
-   company-idle-delay 0.2)
-  (general-def company-active-map
+   company-tooltip-limit 5)
+  (define-advice company-dabbrev--filter (:around (fn &rest args) more-filter)
+    "Further filter canndidates provided by `company-dabbrev'/`company-dabbrev-code' backend.
+Currently this removes long candidate that contains non-latin
+chars.  This should be useful for CJK users."
+    (let ((cands (apply fn args))
+          (exclude (lambda (cand)
+                     (or (and (string-match (rx (not (category latin))) cand)
+                              (> (length cand) 12))))))
+      (cl-remove-if exclude cands)))
+  (define-advice company-pseudo-tooltip-show (:around (fn &rest args) dont-hide-text)
+    "Don't let company tooltip mask my text.
+You'll notice when the tooltip pops up, the text below it are
+pushed down.  This is my desired behavior, because:
+
+- The tooltip pops up while I'm typing.  I don't explicitely
+  control it.
+- I may want to read the code below current line at anytime while
+  typing."
+    (cl-letf* ((make-overlay-orig
+                (symbol-function 'make-overlay))
+               ((symbol-function 'make-overlay)
+                (lambda (beg end &rest args)
+                  (apply make-overlay-orig beg beg args))))
+      (apply fn args)))
+  (define-advice company-buffer-lines (:around (fn &rest _) dont-hide-text)
+    "See the `dont-hide-text' advice around `company-pseudo-tooltip-show'."
+    nil)
+  ;; NOTE: "RET" and "TAB" are for terminal Emacs (equivalent to "C-m" and
+  ;; "C-i"), while "<return>" and "<tab>" are for GUI Emacs.
+  (general-unbind
+    :keymaps 'company-active-map
+    "C-s" "RET" "<return>")
+  (general-def
+    :keymaps 'company-active-map
     "C-p" 'company-select-previous
-    "C-n" 'company-select-next)
-  (defun toki/set-prog-company-backend ()
-    (set (make-local-variable 'company-backends)
-         '(company-dabbrev-code)))
-  (add-hook 'prog-mode-hook #'toki/set-prog-company-backend))
+    "C-n" 'company-select-next
+    "TAB" 'company-complete
+    "<tab>" 'company-complete))
 
 ;;; Ctags
 
