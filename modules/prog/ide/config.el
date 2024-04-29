e1867f9f993c19b0edb8752976e3b351f62d5675
AmaiKinono <amaikinono@gmail.com>
2024-04-30 00:19:10 +0800

    ide, fix: a keybind

--- modules/prog/ide/config.el
+++ modules/prog/ide/config.el
@@ -104,7 +104,7 @@ chars.  This should be useful for CJK users."
   "f" '(flycheck-mode :wk "<> Flycheck Mode")
   "F" '(flycheck-list-errors :wk "List Flycheck Errors")
   "p" '(citre-ace-peek :wk "Peek Definition")
-  "r" '(citre-ace-peek-references :wk "Peek Reference")
+  "r" '(citre-ace-peek-reference :wk "Peek Reference")
   "u" '(citre-update-this-tags-file :wk "Update Tags File")
   "U" '(citre-global-update-database :wk "Update Global DB")
   "C" '(clue-copy :wk "Copy Location")
