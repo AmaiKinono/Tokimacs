a1e87890dfa3ad2eab43e7d6c872888e023f2cb0
AmaiKinono <amaikinono@gmail.com>
2024-05-24 19:51:51 +0800

    facets, tweak: keybinds

--- modules/app/facets/config.el
+++ modules/app/facets/config.el
@@ -4,14 +4,8 @@
   :straight nil
   :defer t
   :init
-  (add-hook 'find-file-hook #'facets-auto-enable-facets-mode)
-  :config
-  (toki-local-def
-    :keymaps 'facets-mode-map
-    "l" '("Insert Link" . facets-insert-link)
-    "L" '("List References" . facets-list-references)
-    "c" '("Copy ID as Link" . facets-copy-id-as-link)
-    "b" '("Jump Back" . facets-jump-back)))
+  (add-hook 'find-file-hook #'facets-auto-enable-facets-mode))
 
 (toki-app-def
-  "f" '(facets-new-facet :wk "New Facet"))
+  "f" '(facets-new-facet :wk "New Facet")
+  "F" '(facets-find-facet :wk "Find Facet"))
